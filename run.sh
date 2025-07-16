e8448132f6984fd8ad90685b4fb00ed186121fab
Aamogh <120258212+aamoghS@users.noreply.github.com>
2025-07-16 18:27:49 -0400

    oops update

--- run.sh
+++ run.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 set -e
 
-# Initialize conda
 source "$(conda info --base)/etc/profile.d/conda.sh"
 
-# Check if env exists, create if not
 if ! conda info --envs | awk '{print $1}' | grep -Fxq "pv-extraction-env"; then
-    echo "Environment not found. Creating it now..."
+    echo "creating"
     conda env create -f environment.yml
 else
-    echo "Environment found. Activating..."
+    echo "starting"
 fi
 
-# Activate the environment
 conda activate pv-extraction-env
-
-# Open the Marimo notebook in editor mode
 marimo edit PV-Work/mainScript/extraction.py
