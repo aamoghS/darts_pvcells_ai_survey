504d502bf31a12a2ad1eabb4d0c703a99738aa2d
Aamogh <120258212+aamoghS@users.noreply.github.com>
2025-07-18 21:58:30 -0400

    should work?

--- run.sh
+++ run.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
 set -e
 
-source "$(conda info --base)/etc/profile.d/conda.sh"
+if command -v conda >/dev/null 2>&1; then 
+    source "$(conda info --base)/etc/profile.d/conda.sh"
 
-if ! conda info --envs | awk '{print $1}' | grep -Fxq "pv-extraction-env"; then
-    echo "creating"
-    conda env create -f environment.yml
-else
-    echo "starting"
+    if ! conda info --envs | awk '{print $1}' | grep -Fxq "pv-extraction-env"; then
+        echo "creating"
+        conda env create -f environment.yml
+    else
+        echo "starting"
+    fi
+
+    conda activate pv-extraction-env
+else 
+    echo "no conda found creating a venv"
+
+    VENV_DIR = ".venv"
+
+    if [! -d "$VENV_DIR"]; then
+        echo "creating a venv"
+        python -m venv $VENV_DIR
+    else 
+        echo "using the current"
+    fi
+
+    source "$VENV_DIR/bin/activate"
+
+    if [ -f "requrirements.txt"]; then 
+        echo "installing"
+        pip install -r requirements.txt
+    else 
+        echo "not found"
+    fi 
 fi
 
-conda activate pv-extraction-env
 marimo edit PV-Work/mainScript/extraction.py
